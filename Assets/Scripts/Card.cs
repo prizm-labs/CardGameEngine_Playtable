6150a00377bdc1e8729f43580c51270f1ce73321
zhoubw <bz2118111@gmail.com>
2016-07-25 12:01:23 -0700

    ok

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+public class Card : MonoBehaviour {
+	public GameObject collidedObject;
+	public int DeckLayer = 8;
+
+	void OnEnable() {
+		GetComponent<ReleaseGesture> ().Released += ReleaseHandler;
+		GetComponent<TapGesture> ().Tapped += TapHandler;
+	}
+
+	void OnDisable() {
+		GetComponent<ReleaseGesture> ().Released -= ReleaseHandler;
+		GetComponent<TapGesture> ().Tapped -= TapHandler;
+	}
+
+
+	void OnTriggerEnter(Collider col) {
+		/*
+		if (col.gameObject.name == "Deck") {
+			Debug.LogError ("Collided with deck");
+		}
+		*/
+		collidedObject = col.gameObject;
+		/*
+		 if (collidedObject.layer == DeckLayer) {
+			collidedObject.GetComponent<Deck> ().highlight (Deck.addCardHighlight);
+		}
+		*/
+	}
+
+	void OnTriggerExit(Collider col) {
+		/*
+		if (col.gameObject.name == "Deck") {
+			Debug.LogError ("Collided with deck");
+		}
+		*/
+		/*
+		if (collidedObject.layer == DeckLayer) {
+			collidedObject.GetComponent<Deck> ().highlight (Deck.defaultHighlight);
+		}
+		*/
+		collidedObject = null;
+	}
+
+	public void ReleaseHandler (object sender, System.EventArgs e) {
+		if (collidedObject != null && collidedObject.layer == DeckLayer) {
+			collidedObject.GetComponent<Deck> ().add (gameObject);
+			//collidedObject.GetComponent<Deck> ().highlight (Deck.defaultHighlight);
+		}
+	}
+
+	public void TapHandler (object sender, System.EventArgs e) {
+		iTween.RotateAdd (gameObject, iTween.Hash ("x", 0, "y", 180, "z", 0));
+	}
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject testDeck;
+	public GameObject testCard;
+
+
+	void Awake() {
+		for (int i = 0; i < 52; i++) {
+			/*GameObject newCard = (GameObject) Instantiate (testCard, testDeck.transform.position, testCard.transform.rotation);
+			testDeck.GetComponent<Deck> ().add (newCard);
+			testDeck.GetComponent<Deck> ().register (newCard);
+			*/
+			testDeck.GetComponent<Deck> ().createCard ();
+		}
+	}
+}
